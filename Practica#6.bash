39932b22f0082d097233ce389af79f3917263dd9
Henry <contrerasenrique973@gmail.com>
2020-07-06 23:20:19 +0000

    decimalToOctal Listo

--- Practica#6.bash
+++ Practica#6.bash
@@ -39,9 +39,41 @@ octaltoDecimal () {
 hexaToDecimal() {
     echo "  HEXADECIMAL -> DECIMAL"
     echo -n "Introduzca hexadecimal: "
-    read hexa
+    read hex
 
-    read pause
+	#Begin
+	len=$(echo -n $hex | wc -m)
+	len=$((len-1))
+	i=0
+	power=$len
+	while [ $i -le $len ];
+	do
+		if [ ${hex:i:1}=A ] || [ ${hex:i:1}=B ] || [ ${hex:i:1}=C ] || [ ${hex:i:1}=D ] | [ ${hex:i:1}=E ] || [ ${hex:i:1}=F ]; #Validating chars
+		then
+			case ${hex:i:1} in
+					A) decimal=$((decimal+(10*(16**power))));;
+					B) decimal=$((decimal+(11*(16**power))));;
+					C) decimal=$((decimal+(12*(16**power))));;
+					D) decimal=$((decimal+(13*(16**power))));;
+					E) decimal=$((decimal+(14*(16**power))));;
+					F) decimal=$((decimal+(15*(16**power))));;
+			esac
+			power=$((power-1))
+			i=$((i+1))
+			continue
+		fi
+		
+		if [ ${hex:i:1} -gt 0 ] && [ ${hex:i:1} -lt 10 ];
+		then
+			num=${hex:i:1}
+			decimal=$((decimal+(num*(16**power))))
+		fi
+		power=$((power-1))
+		i=$((i+1))
+	done
+	echo $decimal
+	decimal=0
+	read pausa
 }
 
 decimalToBinary() {
@@ -61,13 +93,29 @@ decimalToBinary() {
     read pause
 }
 
+decimalToOctal() {
+    echo "  DECIMAL -> OCTAL"
+    echo -n "Introduzca decimal: "
+    read dec
+
+    octal=""
+    while [ $dec -ge 8 ]; do
+        sobra=$(($dec % 8))
+        octal=$sobra$octal
+        dec=$(($dec / 8))
+    done 
+    octal=$dec$octal
+    echo 'Octal: ' $octal
+    read pause
+}
+
 # <!---   MAIN    --->
 option=0
 while [ $option != 9 ]; do
     echo "[1] Convertir un numero Binario a Decimal"
     echo "[2] Convertir un numero Octal a Decimal"
     echo "[3] Convertir un numero Hexadecimal a Decimal"  # PENDIENTE
-    echo "[4] Convertir un numero Decimal a Binario"      # PENDIENTE
+    echo "[4] Convertir un numero Decimal a Binario"      
     echo "[5] Convertir un numero Decimal a Octal"
     echo "[6] Convertir un numero Decimal a Hexadecimal"
     echo "[7] Convertir un numero Binario a Hexadecimal"
@@ -82,7 +130,7 @@ while [ $option != 9 ]; do
         2) octaltoDecimal;;
         3) hexaToDecimal;;
         4) decimalToBinary;;
-        5) ;;
+        5) decimalToOctal;;
         6) ;;
         7) ;;
         8) ;;
